522ec61da34a222c5fed9fb5e8cabc02c1dfe0ed
loorr <zjianfa@qq.com>
2022-08-11 14:02:40 +0800

    add: more

--- ddl/account-center.sql
+++ ddl/account-center.sql
@@ -18,44 +18,44 @@ UNIQUE KEY `email` (`email`)
 ) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8 COMMENT='用户账户';
 
 CREATE TABLE `role` (
-`id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '自增id',
-`db_create_time` datetime(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3) COMMENT '数据库插入时间',
-`db_modify_time` datetime(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3) ON UPDATE CURRENT_TIMESTAMP(3) COMMENT '数据库更新时间',
-`name` varchar(50) NOT NULL COMMENT '角色名',
-`remark` varchar(255) DEFAULT NULL COMMENT '角色备注',
-`valid` tinyint(1) unsigned NOT NULL DEFAULT '1' COMMENT '是否有效',
-PRIMARY KEY (`id`),
-UNIQUE KEY `name` (`name`)
+    `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '自增id',
+    `db_create_time` datetime(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3) COMMENT '数据库插入时间',
+    `db_modify_time` datetime(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3) ON UPDATE CURRENT_TIMESTAMP(3) COMMENT '数据库更新时间',
+    `name` varchar(50) NOT NULL COMMENT '角色名',
+    `remark` varchar(255) DEFAULT NULL COMMENT '角色备注',
+    `valid` tinyint(1) unsigned NOT NULL DEFAULT '1' COMMENT '是否有效',
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `name` (`name`)
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COMMENT='角色';
 
 CREATE TABLE `relate_account_role` (
-`id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '自增id',
-`db_create_time` datetime(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3) COMMENT '数据库插入时间',
-`db_modify_time` datetime(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3) ON UPDATE CURRENT_TIMESTAMP(3) COMMENT '数据库更新时间',
-`uid` bigint(20) unsigned NOT NULL COMMENT 'uid',
-`role` varchar(50) NOT NULL COMMENT '角色',
-PRIMARY KEY (`id`),
-UNIQUE KEY `unique_index` (`uid`,`role`) USING BTREE
+    `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '自增id',
+    `db_create_time` datetime(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3) COMMENT '数据库插入时间',
+    `db_modify_time` datetime(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3) ON UPDATE CURRENT_TIMESTAMP(3) COMMENT '数据库更新时间',
+    `uid` bigint(20) unsigned NOT NULL COMMENT 'uid',
+    `role` varchar(50) NOT NULL COMMENT '角色',
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `unique_index` (`uid`,`role`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户账户';
 
 create table `relate_role_permission`(
-`id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '自增id',
-`db_create_time` datetime(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3) COMMENT '数据库插入时间',
-`db_modify_time` datetime(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3) ON UPDATE CURRENT_TIMESTAMP(3) COMMENT '数据库更新时间',
-`r_id` bigint(20) unsigned NOT NULL COMMENT '角色id',
-`p_id` bigint(20) unsigned NOT NULL COMMENT '权限id',
-unique key `uni_r_p_id` (`r_id`,`p_id`) USING BTREE
-primary key(id)
+    `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '自增id',
+    `db_create_time` datetime(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3) COMMENT '数据库插入时间',
+    `db_modify_time` datetime(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3) ON UPDATE CURRENT_TIMESTAMP(3) COMMENT '数据库更新时间',
+    `r_id` bigint(20) unsigned NOT NULL COMMENT '角色id',
+    `p_id` bigint(20) unsigned NOT NULL COMMENT '权限id',
+    unique key `uni_r_p_id` (`r_id`,`p_id`) USING BTREE
+    primary key(id)
 )ENGINE=InnoDB  comment 'role permission';
 
 create table `permission`(
-`id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '自增id',
-`db_create_time` datetime(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3) COMMENT '数据库插入时间',
-`db_modify_time` datetime(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3) ON UPDATE CURRENT_TIMESTAMP(3) COMMENT '数据库更新时间',
-`name` varchar(100) NOT NULL COMMENT '权限名',
-`remark` varchar(255) DEFAULT NULL COMMENT '权限备注',
-`valid` tinyint(1) unsigned NOT NULL DEFAULT '1' COMMENT '是否有效',
-primary key (id)
+    `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '自增id',
+    `db_create_time` datetime(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3) COMMENT '数据库插入时间',
+    `db_modify_time` datetime(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3) ON UPDATE CURRENT_TIMESTAMP(3) COMMENT '数据库更新时间',
+    `name` varchar(100) NOT NULL COMMENT '权限名',
+    `remark` varchar(255) DEFAULT NULL COMMENT '权限备注',
+    `valid` tinyint(1) unsigned NOT NULL DEFAULT '1' COMMENT '是否有效',
+    primary key (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='权限表';
 
 create table `account_group`(
