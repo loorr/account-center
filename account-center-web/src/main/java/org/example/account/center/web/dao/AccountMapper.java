f8c8a2b11dc88cd0dad17839fc1bcb71822a5643
loorr <zjianfa@qq.com>
2022-08-25 11:47:24 +0800

    add: api

--- account-center-web/src/main/java/org/example/account/center/web/dao/AccountMapper.java
+++ account-center-web/src/main/java/org/example/account/center/web/dao/AccountMapper.java
@@ -5,11 +5,7 @@ import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.Update;
-import org.example.account.center.api.entity.UserGroupItem;
-import org.example.account.center.web.model.Account;
-import org.example.account.center.web.model.AccountExt;
-import org.example.account.center.web.model.Permission;
-import org.example.account.center.web.model.Role;
+import org.example.account.center.web.model.*;
 
 import java.util.List;
 
@@ -51,25 +47,64 @@ public interface AccountMapper {
             "</script>")
     Page<Account> getAccounts(Account account);
 
-    List<UserGroupItem> getUserGroups(Long uid);
+    @Select("<script>" +
+            "select * from "+
+            "(select * from user_group ug where tenant_id=#{tenantId} ) as a "+
+            "join " +
+            "(select ug_id from relate_account_user_group raug  where uid = #{uid}) as b "+
+            "a.id = b.ug_id " +
+            "</script>")
+    List<AccountGroup> getUserGroups(@Param("uid")Long uid, @Param("tenantId")  Long tenantId);
 
     @Select("<script>" +
-            "select r.name, r.id, r.parent_id from " +
-            "(select r_id from  relate_account_role ra where ra.uid=#{uid}) as b " +
-            "join " +
-            "(select * from `role` b where b.tenant_id=#{tenantId}) as r " +
-            "on r.id =b.r_id  " +
-            "<if test='parentIds!=null'>" +
-            "<foreach collection='parentIds' item='parentId' open=' where parent_id in (' separator=',' close=')'>#{parentId}</foreach>" +
-            "</if>" +
-            "<script>")
-    List<Role> getUserDefaultRoles(@Param("uid") Long uid, @Param("tenantId") Long tenantId,@Param("parentIds") List<Long> parentIds);
+            "select * from user_group ug where ug.tenant_id = #{tenantId}  and parent_id in " +
+            "<foreach item='item' index='index' collection='ids' open='(' separator=',' close=')'>#{item}</foreach>" +
+            "</script>")
+    List<AccountGroup> getUserGroupsByParentId( @Param("parentIds") List<Long> parentIds, @Param("tenantId")  Long tenantId);
+
+    @Select("<script>" +
+        "select r.name, r.id, r.parent_id from " +
+        "(select r_id from  relate_account_role ra where ra.uid=#{uid}) as b " +
+        "join " +
+        "(select * from `role` b where b.tenant_id=#{tenantId}) as r " +
+        "on r.id =b.r_id  " +
+        "<if test='parentIds!=null'>" +
+        "<foreach collection='parentIds' item='parentId' open=' where parent_id in (' separator=',' close=')'>#{parentId}</foreach>" +
+        "</if>" +
+        "</script>" )
+    List<Role> getUserDefaultRoles(@Param("uid") Long uid, @Param("tenantId") Long tenantId, @Param("parentIds") List<Long> parentIds);
 
 
-    @Select("")
-    List<Permission> getPermissionsByRoleId(@Param("roleIds") Long roleIds);
+    @Select("<script>" +
+        "select * from  " +
+        "(select * from relate_user_group_role rugr where ug_id in " +
+        "<foreach item='item' index='index' collection='ids' open='(' separator=',' close=')'>#{item}</foreach>" +
+        " ) as a " +
+        "join  " +
+        "(select * from `role` where tenant_id = 10001) as b " +
+        "on b.id = a.r_id "+
+        "</script>" )
+    List<Role> getRoleByAccountGroupIds(@Param("accountGroupIds") List<Long> accountGroupIds);
 
-    @Select("")
-    List<Permission> getPermissionsByParentId(@Param("parentIds") List<Long> parentIds);
+    @Select("<script>" +
+            "select * from role p where p.tenant_id=#{tenantId} and p.parent_id in " +
+            "<foreach collection='parentIds' item='parentId' open='(' separator=',' close=')'>#{parentId}</foreach>" +
+            "</script>")
+    List<Role> getRoleByParentId(@Param("parentIds") List<Long> parentIds, @Param("tenantId") Long tenantId);
+
+    @Select("<script>" +
+            "select * from (select p_id from relate_role_permission rrp where r_id in " +
+            "<foreach collection='roleIds' item='roleId' open='(' separator=',' close=')'>#{roleId}</foreach>" +
+            ") as a join " +
+            "(select * from permission p where p.tenant_id=#{tenantId}) as b " +
+            "on a.p_id = b.id " +
+            "</script>")
+    List<Permission> getPermissionsByRoleId(@Param("roleIds") List<Long> roleIds, @Param("tenantId") Long tenantId);
+
+    @Select("<script>" +
+            "select * from permission p where p.tenant_id=#{tenantId} and p.parent_id in " +
+            "<foreach collection='parentIds' item='parentId' open='(' separator=',' close=')'>#{parentId}</foreach>" +
+            "</script>")
+    List<Permission> getPermissionsByParentId(@Param("parentIds") List<Long> parentIds, @Param("tenantId") Long tenantId);
 
 }
--- account-center-web/src/main/java/org/example/account/center/web/service/impl/AccountServiceImpl.java
+++ account-center-web/src/main/java/org/example/account/center/web/service/impl/AccountServiceImpl.java
@@ -9,10 +9,18 @@ import org.example.account.center.api.entity.req.ModifyUserInfoReq;
 import org.example.account.center.api.common.AccountErrorCode;
 import org.example.account.center.web.dao.AccountMapper;
 import org.example.account.center.web.model.Account;
+import org.example.account.center.web.model.AccountGroup;
+import org.example.account.center.web.model.Permission;
+import org.example.account.center.web.model.Role;
 import org.example.account.center.web.service.AccountService;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
 
 import javax.annotation.Resource;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 public class AccountServiceImpl implements AccountService {
@@ -43,11 +51,73 @@ public class AccountServiceImpl implements AccountService {
         result.setUid(account.getUid());
         result.setNickname(account.getNickname());
         result.setUsername(account.getUsername());
-
-
-
         return result;
     }
+
+    private List<Role> getDefaultRoles(Long uid, Long tenantId) {
+        List<Role> roles = accountMapper.getUserDefaultRoles(uid, tenantId, null);
+        Map<Long, Role> roleMap = roles.stream().collect(Collectors.toMap(Role::getId, role -> role, (k1, k2) -> k1));
+
+        List<Long> rootIds = roles.stream().filter(role -> role.getParentId() == null).map(Role::getId).collect(Collectors.toList());
+        int i = 0;
+        while (CollectionUtils.isEmpty(rootIds) && i < 10){
+            roles = accountMapper.getUserDefaultRoles(uid, tenantId, rootIds);
+            roleMap.putAll(roles.stream().collect(Collectors.toMap(Role::getId, role -> role, (oldValue, newValue) -> newValue)));
+            rootIds = roles.stream().filter(role -> role.getParentId() == null).map(Role::getId).collect(Collectors.toList());
+            i ++;
+        }
+        return roleMap.values().stream().collect(Collectors.toList());
+    }
+
+
+    private List<AccountGroup> getAccountGroup(Long uid, Long tenantId){
+        List<AccountGroup> accountGroups = accountMapper.getUserGroups(uid, tenantId);
+        Map<Long, AccountGroup> accountGroupMap = accountGroups.stream().collect(Collectors.toMap(AccountGroup::getId, accountGroup -> accountGroup, (k1, k2) -> k1));
+        List<Long> rootIds = accountGroups.stream().filter(accountGroup -> accountGroup.getParentId() == null).map(AccountGroup::getId).collect(Collectors.toList());
+        int i = 0;
+        while (CollectionUtils.isEmpty(rootIds) && i < 10){
+            accountGroups = accountMapper.getUserGroupsByParentId(rootIds, tenantId);
+            accountGroupMap.putAll(accountGroups.stream().collect(Collectors.toMap(AccountGroup::getId, accountGroup -> accountGroup, (oldValue, newValue) -> newValue)));
+            rootIds = accountGroups.stream().filter(accountGroup -> accountGroup.getParentId() == null).map(AccountGroup::getId).collect(Collectors.toList());
+            i ++;
+        }
+        return accountGroupMap.values().stream().collect(Collectors.toList());
+    }
+
+    private List<Role> getRolesByAccountGroup(List<Long> groupIds, Long tenantId){
+        if (CollectionUtils.isEmpty(groupIds)){
+            return Collections.emptyList();
+        }
+        List<Role> roles = accountMapper.getRoleByAccountGroupIds(groupIds);
+        Map<Long, Role> roleMap = roles.stream().collect(Collectors.toMap(Role::getId, role -> role, (k1, k2) -> k1));
+        List<Long> rootIds = roles.stream().filter(role -> role.getParentId() == null).map(Role::getId).collect(Collectors.toList());
+        int i = 0;
+        while (CollectionUtils.isEmpty(rootIds) && i < 10){
+            roles = accountMapper.getRoleByParentId(rootIds, tenantId);
+            roleMap.putAll(roles.stream().collect(Collectors.toMap(Role::getId, role -> role, (oldValue, newValue) -> newValue)));
+            rootIds = roles.stream().filter(role -> role.getParentId() == null).map(Role::getId).collect(Collectors.toList());
+            i ++;
+        }
+        return roleMap.values().stream().collect(Collectors.toList());
+    }
+
+    private List<Permission> getPermissionByRoleIds(List<Long> roleIds, Long tenantId){
+        if (CollectionUtils.isEmpty(roleIds)){
+            return Collections.emptyList();
+        }
+        List<Permission> permissions = accountMapper.getPermissionsByRoleId(roleIds, tenantId);
+        Map<Long, Permission> permissionMap = permissions.stream().collect(Collectors.toMap(Permission::getId, permission -> permission, (k1, k2) -> k1));
+        List<Long> rootIds = permissions.stream().filter(permission -> permission.getParentId() == null).map(Permission::getId).collect(Collectors.toList());
+        int i = 0;
+        while (CollectionUtils.isEmpty(rootIds) && i < 10){
+            permissions = accountMapper.getPermissionsByParentId(rootIds, tenantId);
+            permissionMap.putAll(permissions.stream().collect(Collectors.toMap(Permission::getId, permission -> permission, (oldValue, newValue) -> newValue)));
+            rootIds = permissions.stream().filter(permission -> permission.getParentId() == null).map(Permission::getId).collect(Collectors.toList());
+            i ++;
+        }
+        return permissionMap.values().stream().collect(Collectors.toList());
+    }
+
 
     @Override
     public AccountVo getAccountInfo(GetAccountInfoReq req) {
